990f2feebb540dc68a7a373eb763d0c203872a65
Akshat Dubey <akshatdubey0808@gmail.com>
2024-10-10 15:53:21 +0530

    added retries and backoffs to prevent timeouts on initial requests

--- app/(dashboard)/group/[groupId]/_actions/leetcode-actions.ts
+++ app/(dashboard)/group/[groupId]/_actions/leetcode-actions.ts
@@ -5,30 +5,50 @@ interface LeetcodeIdAndName {
   name: string;
 }
 
+async function fetchWithRetry(url: string, options: RequestInit, retries = 3, backoff = 300) {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response;
+  } catch (error) {
+    if (retries > 0) {
+      await new Promise(resolve => setTimeout(resolve, backoff));
+      return fetchWithRetry(url, options, retries - 1, backoff * 2);
+    } else {
+      throw error;
+    }
+  }
+}
+
 export async function getLeetcodeUserData(
   leetcodeIdAndName: LeetcodeIdAndName[],
 ) {
   const leetcodeUserData = await Promise.all(
     leetcodeIdAndName.map(async (user) => {
-      const response = await fetch(
-        `${process.env.LEETCODE_API}/userProfile/${user.id}`,
-        {
-          cache: "force-cache", // Force cache for the response
-          next: {
-            revalidate: 600, // Revalidate every 10 minutes
-          },
-        }
-      );
+      try {
+        const response = await fetchWithRetry(
+          `${process.env.LEETCODE_API}/userProfile/${user.id}`,
+          {
+            next: {
+              revalidate: 600, // Revalidate every 10 minutes
+            },
+          }
+        );
 
-      if (!response.ok) {
-        throw new Error(`Failed to fetch data for user ${user.id}`);
+        const data = await response.json();
+        return {
+          ...data,
+          name: user.name,
+        };
+      } catch (error: any) {
+        console.error(`Error fetching data for user ${user.id}:`, error);
+        return {
+          error: `Failed to fetch data for user ${user.id}`,
+          name: user.name,
+        };
       }
-
-      const data = await response.json();
-      return {
-        ...data,
-        name: user.name,
-      };
     }),
   );
 
